7df180530c46c7f3ddef8fe20b6ee2dd1cd3d6a8
der-ferdi <ferdinand.ebner@posteo.de>
2021-03-25 16:26:36 +0100

    Korrektur Musterlösung

--- nz/main.js
+++ nz/main.js
@@ -1,17 +1,15 @@
 
+
+
 const map = L.map("map", {
-center: [-43.733333, 170.095556],
-zoom: 13,
-layers: [
-    L.titleLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
-]
-});
-
-let mrk = L.marker([-43.733333, 170.095556]) .addTo(map);
-mrk.bindPopup("Tongariro National Park") .openPopup();
-
-
-
-console.log(document.querySelector("#map"));
-
-
+    center: [-43.733333, 170.095556],
+    zoom: 13,
+    layers: [
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+    ]
+  });
+  
+  let mrk = L.marker([-43.733333, 170.095556]).addTo(map);
+  mrk.bindPopup('Tongariro National Park');
+  
+  console.log(document.querySelector("#map"));
