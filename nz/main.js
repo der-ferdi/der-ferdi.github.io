380bc53b79465325269f27cd81a6c4b48ac9ab2b
der-ferdi <ferdinand.ebner@posteo.de>
2021-03-25 16:14:04 +0100

    Karte weiter

--- nz/main.js
+++ nz/main.js
@@ -1,2 +1,14 @@
 console.log("Hello World!");
 console.log(L);
+
+const map = L.map("map", {
+center: [-43.733333, 170.095556],
+zoom: 13,
+layers: [
+    L.titleLayer("https://{s}.title.openstreetmap.org/{z}/{x}/{y}.png")
+]
+});
+//-43.733333, 170.095556
+console.log(document.querySelector("#map"));
+
+
