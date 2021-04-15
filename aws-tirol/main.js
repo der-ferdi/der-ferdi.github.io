d9ee0aa4e6aedff8fb92c29c1974f964f1fed403
der-ferdi <ferdinand.ebner@posteo.de>
2021-04-15 16:17:09 +0200

    Karte Layer hinzufügen

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -1,4 +1,5 @@
 
+
 let basemapGray = L.tileLayer.provider('BasemapAT.grau');
 
 let map = L.map("map", {
@@ -11,5 +12,10 @@ let map = L.map("map", {
 
 let layerControl = L.control.layers({
     "BasemapAT.grau": basemapGray,
-    "BasemapAT.orthofoto": L.tileLayer.provider('BasemapAT.orthofoto')
+    "BasemapAT.orthofoto": L.tileLayer.provider('BasemapAT.orthofoto'),
+    "BasemapAT.surface": L.tileLayer.provider('BasemapAT.surface'),
+    "BasemapAT.overlay+ortho": L.layerGroup([
+        L.tileLayer.provider('BasemapAT.orthofoto'),
+        L.tileLayer.provider('BasemapAT.overlay')
+    ])
 }).addTo(map);
