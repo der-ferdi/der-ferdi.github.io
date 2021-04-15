05026d23782bb1b7a46323c0abc54a1859cdfbb2
der-ferdi <ferdinand.ebner@posteo.de>
2021-04-15 17:33:30 +0200

    aktuelle Daten

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -39,8 +39,13 @@ fetch(awsUrl)
             marker.bindPopup(`
             <h3>${station.properties.name}</h3>
             <ul>
-                <li>Datum: ${formattedDate.toLocaleString("de")}</li>
-                <li>Temperatur: ${station.properties.LT} C</li>
+            <li>Datum: ${formattedDate.toLocaleString("de")}</li>
+            <li>Seehöhe: ${station.geometry.coordinates[2]} m</li>
+            <li>Temperatur: ${station.properties.LT} C</li>
+            <li>Schneehöhe: ${station.properties.HS || '?'} cm</li>
+            <li>Windgeschwindigkeit: ${station.properties.WG || '?'} km/h</li>
+            <li>Windgeschwindrichtung: ${station.properties.WR || '?'}</li>
+
                 </ul>
             `);
             marker.addTo(awsLayer);
